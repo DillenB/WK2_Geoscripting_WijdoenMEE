7067ca357ece07289027d78082d70452d742ac73
Heumen <rik.vanheumen@wur.nl>
2017-01-19 10:51:03 +0100

    Assignment 8 Wij doen MEE_ Dillen Bruil and Rik van Heumen Final version

--- Lesson8/R/rmse.R
+++ Lesson8/R/rmse.R
@@ -1,3 +1,10 @@
+###################
+# Wij doen MEE    #
+# 18 January 2017 #
+###################
+
+#Function to calculate the RMSE
+
 rmse <- function (predicted, actual){
   rmse <- sqrt(mean((predicted - actual)^2, na.rm=T))
   return(rmse)
--- Lesson8/main.R
+++ Lesson8/main.R
@@ -7,10 +7,17 @@
 
 rm(list=ls())
 
+#Load libraries
+
 library(raster)
 library(rasterVis)
+library(knitr)
+
+
+#Load source files
 
 source("R/rmse.R")
+
 
 #Load data
 load("data/GewataB1.rda")
@@ -22,13 +29,13 @@ load("data/GewataB7.rda")
 load("data/vcfGewata.rda")
 load("data/trainingPoly.rda")
 
-#The loaded Landsat7 data
-all <- c(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7,vcfGewata)
-GewataBrick <- brick(all)
-names(GewataBrick) <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
 
-#hist(gewataBrick)
-dev.off()
+#The loaded Landsat7 data
+headings <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
+all_uncut <- c(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7,vcfGewata)
+GewataBrick_uncut <- brick(all_uncut)
+names(GewataBrick_uncut) <- headings
+
 
 #Outliers cut off from the loaded Landsat7 data
 GewataB1[GewataB1 > 750]  <- NA
@@ -39,39 +46,43 @@ GewataB5[GewataB5 > 3500]  <- NA
 GewataB7[GewataB7 > 2200]  <- NA
 vcfGewata[vcfGewata > 100] <- NA
 
-
+#Make the brick
 all <- c(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7,vcfGewata)
 GewataBrick <- brick(all)
-names(GewataBrick) <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
+names(GewataBrick) <- headings
 
-#hist(GewataBrick)
-#pairs(GewataBrick)
-#dev.off()
+#Plot the pairs to study the correlation
+pairs(GewataBrick)
 
+#Build the model
 df <- as.data.frame(getValues(GewataBrick))
 vcfmodel <- lm(VCF ~ band1 + band2 + band3 + band4 + band5 + band7, data = df)
 vcfpred_map <- predict(GewataBrick, model=vcfmodel)
 summary(vcfmodel)
-summary(vcfpred_map)
 vcfpred_map[vcfpred_map < 0] <- NA
 
+#plot the outcome of the model vs the actual data
 par(mfrow=c(1,2))
 plot(vcfpred_map, main = "Predicted VCF")
 plot(vcfGewata, main = "Gewata VCF")
 
-
-levelplot(stack(vcfpred_map, vcfGewata), main=c("Predicted VCF","Gewata VCF"), 
+#levelplot of the above
+levelplot(stack(vcfpred_map, vcfGewata), main=c("Predicted VCF","Gewata VCF"),
           col.regions=rev(grDevices::terrain.colors(100)), axes=T)
 
-
+#calculate the RMSE
 rmse_map <- rmse(vcfpred_map,vcfGewata)
 rmse_avg <- rmse(as.data.frame(vcfpred_map),as.data.frame(vcfGewata))
 
+#Plot the RMSE_map
+par(mfrow=c(1,1))
 plot(rmse_map, main="RMSE")
 
+#Define the classes
 trainingPoly@data$Code <- as.numeric(trainingPoly@data$Class)
 classes <- rasterize(trainingPoly, rmse_map, field='Code')
 
+#Calculate the RMSE for the classes
 rmse_classes <- zonal(rmse_map, classes, "mean")
 rownames(rmse_classes) <- c("Cropland", "Forest", "Wetlands")
 rmse_classes
